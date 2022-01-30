26d334e83e85519847b628737e04f45e3f85bf87
Suguru677 <s.m.yt.939@outlook.jp>
2022-01-31 00:05:18 +0900

    :recycle: change url

--- src/main.js
+++ src/main.js
@@ -5,5 +5,5 @@ import axios from 'axios'
 const app = createApp(App)
 // app.use(VueGoodTablePlugin)
 app.config.globalProperties.axios=axios
-axios.defaults.baseURL = 'http://localhost:5000'
+axios.defaults.baseURL = 'http://localhost:5000/'
 app.mount('#app')
